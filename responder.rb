efce48494a0aec1c1d87ab3058b5d7e262737ca7
Roland Swingler <roland.swingler@gmail.com>
2009-03-22 21:23:23 +0000

    Adding a responder and test

---
+++ responder.rb
@@ -0,0 +1,50 @@
+module Harken
+  # Base class for responding to messages directed at the chat room bot.
+  # This should be subclassed and appropriate actions to be listened to
+  # defined with <code>listen</code>.
+  #
+  # A hello world example:
+  #
+  # <pre><code>
+  # class Hello < Harken::Responder
+  #   listen "greet <person>" do |m|
+  #     "Hello, #{m.person}!"
+  #   end
+  # end
+  # </code></pre>
+  class Responder
+    class << self
+      attr_reader :listeners
+      @@responders = []
+      @@parser = MessageParser.new
+
+      def inherited(klass)
+        @@responders << klass
+        klass.class_eval { @listeners = {} }
+      end
+
+      # Listen for a message and call the given block if it matches the format string.
+      #
+      # Raises an exception if the format string could not be parsed successfully
+      def listen(format, &block)
+        matcher = @@parser.interpret(format)
+        raise "Could not listen for '#{format}'" unless matcher
+        @listeners[matcher] = block
+      end
+      
+      # Returns an array with all Responder subclasses.
+      def all_responders 
+        @@responders
+      end
+    end
+
+    # Receive a message and call any blocks that have been registered with <code>listen</code>
+    # if the message matches the format.
+    def receive(message)
+      self.class.listeners.map do |matcher, block|
+        match = matcher.match(message)
+        block.call(match) if match
+      end.compact
+    end
+  end
+end
---
+++ test_responder.rb
@@ -0,0 +1,48 @@
+require 'treetop'
+require 'test/unit'
+require 'polyglot'
+require 'message'
+require 'node_modules'
+require 'responder'
+
+class SimpleResponder < Harken::Responder
+  listen "test" do
+    "foo"
+  end
+
+  listen "test <variable>" do |m|
+    "value is '#{m.variable}'"
+  end
+end
+
+class DuplicateResponder < Harken::Responder
+  listen "test" do
+    "first"
+  end
+
+  listen "test" do
+    "second"
+  end
+end
+
+class TestResponder < Test::Unit::TestCase
+  def test_simple_responder_responds_to_message
+    assert_equal ["foo"], SimpleResponder.new.receive("test")
+  end
+
+  def test_simple_responder_ignores_message
+    assert_equal [], SimpleResponder.new.receive("blob")
+  end
+  
+  def test_duplicate_responder_responds_to_message
+    assert_equal ["second"], DuplicateResponder.new.receive("test")
+  end
+
+  def test_with_variable
+    assert_equal ["value is 'hello world'"], SimpleResponder.new.receive("test hello world")
+  end
+
+  def test_all_responders_are_tracked
+    assert_equal [SimpleResponder, DuplicateResponder], Harken::Responder.all_responders
+  end
+end

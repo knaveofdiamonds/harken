8814bd4345f115486d1a0562665190eae66c2fa6
Roland Swingler <roland.swingler@gmail.com>
2009-03-29 15:06:27 +0100

    Tweaks to ResponderGroup documentation

--- lib/responder_group.rb
+++ lib/responder_group.rb
@@ -1,12 +1,18 @@
 module Harken
   # Allows you to composite Responders into a group.
-  #
-  # A ResponderGroup shares the same interface as a Responder
   class ResponderGroup
-    def initialize(klasses=Responder.all)
+    # Creates a new ResponderGroup.
+    #
+    # Can be constructed with specific Responder classes or, if left blank
+    # every loaded Responder will be composed.
+    def initialize(*klasses)
+      klasses = Responder.all if klasses.empty?
       @responders = klasses.map {|klass| klass.new }
     end
 
+    # Receive a message from the chat room
+    #
+    # Returns an array of replies for the room
     def receive(message)
       @responders.map {|r| r.receive(message) }.flatten
     end
--- test/test_responder_group.rb
+++ test/test_responder_group.rb
@@ -4,10 +4,16 @@ require 'lib/responder_group'
 require File.dirname(__FILE__) + '/dummy_responders'
 
 class TestResponder < Test::Unit::TestCase
-  def test_responder_group_listens_for_all_responders_by_default
+  def test_responder_group_composes_all_responders_by_default
     replies = Harken::ResponderGroup.new.receive("test")
     assert replies.include?("foo")
     assert replies.include?("second")
     assert ! replies.include?("baz")
+  end
+
+  def test_responder_group_can_be_composed_with_specific_classes
+    replies = Harken::ResponderGroup.new(SimpleResponder).receive("test")
+    assert replies.include?("foo")
+    assert ! replies.include?("second")
   end
 end
